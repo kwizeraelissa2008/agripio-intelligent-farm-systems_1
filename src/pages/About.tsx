472fdb88a67b7be3c005d5e7fbede653e146407f
NIYOBYOSE-Isaac-Precieux <isaprecieux112@gmail.com>
2026-04-14 12:33:40 +0200

    jlkslhlgfhl

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -108,7 +108,7 @@ export default function About() {
         {/* Bottom copyright bar */}
         <div className="text-center py-4 border-t" style={{ borderColor: '#e6efe6' }}>
           <p className="text-[11px] text-gray-400">
-            © 2026 AgriPio Team &nbsp;|&nbsp; Protected under Rwanda IP Law No. 31/2009 &nbsp;|&nbsp; ARIPO Member State
+            © 2026 AgriPio Team &nbsp;
           </p>
           <p className="text-[10px] text-gray-300 mt-0.5">
             Source code, UI design, educational content &amp; AI persona are original creative works. All rights reserved.
